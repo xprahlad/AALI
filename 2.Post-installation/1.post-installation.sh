a3fa7ecdb6c63600dbbfc8b5c30f77b1acc756b5
ictsolved <saradgajurel@gmail.com>
2017-06-26 16:18:16 +0545

    removing scripts after completion

--- 2.Post-installation/1.post-installation.sh
+++ 2.Post-installation/1.post-installation.sh
@@ -97,6 +97,7 @@ echo "7. Fixing screen-tearing"
 sudo mkdir /etc/X11/xorg.conf.d/
 echo -e 'Section "Device"\n Identifier "Intel Graphics"\n Driver "Intel"\n Option "AccelMethod" "sna"\n Option "TearFree" "true"\nEndSection' | sudo tee /etc/X11/xorg.conf.d/20-intel.conf
 echo
+rm -rf ~/AALI-master ~/master.zip
 echo "Arch Linux Setup Completed Successfuly!"
 echo
 echo "Please reboot the computer now."
